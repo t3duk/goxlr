cd0b9835689a63e047b53d4a283dc767c48a1740
Teddy Brine <ted@daad.wtf>
2023-07-04 20:49:31 +0100

    rw

--- app.js
+++
@@ -1,9 +0,0 @@
-const { GeneralCommands, DaemonCommands, GoxlrCommands } = require("./goxlr");
-
-const generalCommands = new GeneralCommands();
-const daemonCommands = new DaemonCommands();
-const goxlrCommands = new GoxlrCommands();
-
-(async () => {
-  goxlrCommands.setFaderMuteState("Mic", true);
-})();
