bf6be6b7aeb602cee183c9dcbe83231914506a21
Ted <me@t3d.uk>
2024-09-22 20:10:07 +0100

    feat: ✨ entry point

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { GetStatus } from "./modules/GetStatus";
+import { GoXLRStatus } from "./types/GoXLRStatus";
+
+export default class GoXLR {
+  address: string;
+  port: string;
+
+  constructor(address?: string, port?: string) {
+    this.address = address || "localhost";
+    this.port = port || "14564";
+  }
+
+  async status(): Promise<GoXLRStatus> {
+    return await GetStatus(this.address, this.port);
+  }
+}
